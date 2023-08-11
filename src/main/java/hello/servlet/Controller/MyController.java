dff3d60b24c17fb018a5e1b4658e89420ca3d4e7
euijin <dmlwls990527@gmail.com>
2023-08-11 16:27:27 +0900

    [refractor] controller 폴더 생성

--- src/main/java/hello/servlet/Controller/MyController.java
+++ src/main/java/hello/servlet/Controller/MyController.java
@@ -4,8 +4,9 @@ import hello.servlet.Service.TritonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.multipart.MultipartFile;
 @RestController
 public class MyController {
 
@@ -19,21 +20,20 @@ public class MyController {
     }
 
     @PostMapping("/사진 업로드")
-    public ImageResponse uploadImage(@RequestBody byte[] imageBytes) {
-       /* String imagePath = imageService.saveImage(imageBytes);
+    public ImageResponse uploadImage(@RequestParam("imageFile") MultipartFile imageFile) {
+        /* String imagePath = imageService.saveImage(imageBytes);
+
         Long imageId = imageService.saveImagePathToDatabase(imagePath); */
 
-        ImageResponse Image = tritonService.sendImageToTritonServer(imageBytes);
-        /*
-     triton server 에 imageBytes 를 보낸 후 Image 라는 이름으로 json 데이터 값을 반환 받음
+        ImageResponse imageResponse = tritonService.sendImageToTritonServer(imageFile);
 
-     String dl_name;
-     String dl_company;
-     String dl_mapping_code;
-      */
+        return imageResponse;
 
-        // 이약은요 api 에 rest api 요청
-
-        return Image; // return 값 json 형태 다시 바꿔야됨
     }
+
+
 }
+
+
+
+
--- src/main/java/hello/servlet/Service/TritonService.java
+++ src/main/java/hello/servlet/Service/TritonService.java
@@ -8,8 +8,10 @@ import org.springframework.http.HttpEntity;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper; // jackson-databind 라이브러리를 추가해야 함
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
 public class TritonService {
@@ -20,35 +22,31 @@ public class TritonService {
     private RestTemplate restTemplate;
 
     @Autowired
-    private ObjectMapper objectMapper; // ObjectMapper를 주입하여 JSON 파싱 사용
+    private ObjectMapper objectMapper;
 
-    public ImageResponse sendImageToTritonServer(byte[] imageBytes) {
+    public ImageResponse sendImageToTritonServer(MultipartFile imageFile) {
         try {
             HttpHeaders headers = new HttpHeaders();
             headers.setContentType(MediaType.MULTIPART_FORM_DATA);
 
+            // 이미지 파일을 바이트 배열로 변환
+            byte[] imageBytes = imageFile.getBytes();
+
+            // MultipartFile을 바로 HttpEntity로 사용 가능
             HttpEntity<byte[]> requestEntity = new HttpEntity<>(imageBytes, headers);
 
             ResponseEntity<String> response = restTemplate.postForEntity(TRITON_URL, requestEntity, String.class);
 
             if (response.getStatusCode().is2xxSuccessful()) {
                 String responseBody = response.getBody();
-                // JSON 응답 파싱
                 JsonNode jsonResponse = objectMapper.readTree(responseBody);
 
-                // 필요한 데이터 추출
-                JsonNode dlCompany = jsonResponse.get("images").get(0).get("dl_company");
-                JsonNode dlName = jsonResponse.get("images").get(0).get("dl_name");
-                JsonNode itemSeq = jsonResponse.get("images").get(0).get("item_seq");
-
-                // ImageResponse 객체 생성 및 데이터 설정
                 ImageResponse imageResponse = new ImageResponse();
-                imageResponse.setDl_company(dlCompany.asText());
-                imageResponse.setDl_name(dlName.asText());
-                imageResponse.setDl_mapping_code(itemSeq.asText());
+                imageResponse.setDl_company(jsonResponse.get("images").get(0).get("dl_company").asText());
+                imageResponse.setDl_name(jsonResponse.get("images").get(0).get("dl_name").asText());
+                imageResponse.setDl_mapping_code(jsonResponse.get("images").get(0).get("dl_mapping_code").asText());
 
                 return imageResponse;
-
             } else {
                 // 에러 처리
             }
